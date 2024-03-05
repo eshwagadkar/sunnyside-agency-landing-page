75c3b13837d12a5b36c90fba1438ae2e7998f8dd
eshwagadkar <eshwagadkar007@gmail.com>
2024-03-05 18:12:58 +0530

    move index.js to src folder

---
+++ app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './scripts/App';
+
+import './styles/main.scss';
+
+if( process.env.NODE_ENV === 'production' ) {
+   console.log('Production Mode');
+} else if ( process.env.NODE_ENV === 'development' ) {
+   console.log('Development Mode');
+}
+
+const domNode = document.querySelector("#root")
+const root = createRoot(domNode)
+root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
+
+
+if(module.hot) { module.hot.accept() }
--- app/src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import '../styles/main.scss';
-
-if( process.env.NODE_ENV === 'production' ) {
-   console.log('Production Mode');
-} else if ( process.env.NODE_ENV === 'development' ) {
-   console.log('Development Mode');
-}
-
-if(module.hot) { module.hot.accept() }
